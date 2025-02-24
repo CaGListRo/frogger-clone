7b7bcc0303761340dc16c6d3deb5b792db034a78
CaGListRo <paco.nyns@gmail.com>
2025-02-24 17:41:40 +0100

    WIP: Corrected the calculations for the offset. Tried to fix the bug in the movement of the frog.

--- frog.py
+++ frog.py
@@ -25,7 +25,9 @@ class Frog:
         self.pos: pg.Vector2 = pg.Vector2(frog_pos)
         self.image: pg.Surface = pg.Surface(image_size, pg.SRCALPHA)
         self.image.blit(self.animation.get_current_image(), (0, 0))
-        self.rect: pg.Rect = self.image.get_rect(topleft=self.pos)
+        self.image_to_blit: pg.Surface = self.image
+        self.image_rect: pg.Rect = self.image.get_rect(center=self.pos)
+        self.collision_rect: pg.Rect = pg.Rect(self.pos.x - 14, self.pos.y - 37, 29, 37)
         self.speed: int = 200
         self.jumping: bool = False
         self.direction: str = direction
@@ -43,46 +45,47 @@ class Frog:
         if self.direction == "south":
             match self.old_direction:
                 case "north":
-                    self.pos.y -= 43
+                    self.pos.y -= 38
                 case "east":
-                    self.pos.x -= 21
-                    self.pos.y -= 21
+                    self.pos.x += 19
+                    self.pos.y -= 19
                 case "west":
-                    self.pos.x += 21
-                    self.pos.y -= 21
+                    self.pos.x -= 19
+                    self.pos.y -= 19
 
         elif self.direction == "north":
             match self.old_direction:
                 case "south":
-                    self.pos.y += 43
+                    self.pos.y += 38
                 case "east":
-                    self.pos.x -= 21
-                    self.pos.y += 21
+                    self.pos.x += 19
+                    self.pos.y += 19
                 case "west":
-                    self.pos.x += 21
-                    self.pos.y += 21
+                    self.pos.x -= 19
+                    self.pos.y -= 19
 
         elif self.direction == "west":
             match self.old_direction:
                 case "north":
-                    self.pos.x -= 21
-                    self.pos.y += 21
+                    self.pos.x += 19
+                    self.pos.y -= 19
                 case "south":
-                    self.pos.x += 21
-                    self.pos.y += 21
+                    self.pos.x += 19
+                    self.pos.y += 19
                 case "east":
-                    self.pos.x -= 43
+                    self.pos.x += 38
             
         elif self.direction == "east":
             match self.old_direction:
                 case "north":
-                    self.pos.x -= 21
-                    self.pos.y -= 21
+                    self.pos.x -= 19
+                    self.pos.y -= 19
                 case "south":
-                    self.pos.x += 21
-                    self.pos.y += 21    
+                    self.pos.x -= 19
+                    self.pos.y += 19    
                 case "west":
-                    self.pos.x += 43
+                    self.pos.x -= 38
+        self.old_direction = self.direction
 
     def update(self, dt: float) -> None:
         """
@@ -94,8 +97,8 @@ class Frog:
         self.animation.update(dt)
         if self.jumping:
             if self.direction == "north":
-                if self.old_direction != self.direction:
-                    self.fix_position()
+                # if self.old_direction != self.direction:
+                #     self.fix_position()
                 self.pos.y -= (self.speed * dt)
                 self.angle = 0
                 if self.pos.y <= self.destination.y:
@@ -103,8 +106,8 @@ class Frog:
                     self.jumping = False
 
             elif self.direction == "south":
-                if self.old_direction != self.direction:
-                    self.fix_position()
+                # if self.old_direction != self.direction:
+                #     self.fix_position()
                 self.pos.y += (self.speed * dt)
                 self.angle = 180
                 if self.pos.y >= self.destination.y:
@@ -112,28 +115,27 @@ class Frog:
                     self.jumping = False
 
             elif self.direction == "west":
-                if self.old_direction != self.direction:
-                    self.fix_position()
+                # if self.old_direction != self.direction:
+                #     self.fix_position()
                 self.pos.x -= (self.speed * dt)
-                self.angle = -90
+                self.angle = 90
                 if self.pos.x <= self.destination.x:
                     self.pos.x = self.destination.x
                     self.jumping = False
 
             elif self.direction == "east":
-                if self.old_direction != self.direction:
-                    self.fix_position()
+                # if self.old_direction != self.direction:
+                #     self.fix_position()
                 self.pos.x += (self.speed * dt)
-                self.angle = 90
+                self.angle = 270
                 if self.pos.x >= self.destination.x:
                     self.pos.x = self.destination.x
                     self.jumping = False
         if old_angle != self.angle:
             self.rotate = True
-        self.old_direction = self.direction
+        
         # update the rect
-        self.rect.x = self.pos.x
-        self.rect.y = self.pos.y
+        self.image_rect = self.image.get_rect(center=self.pos)
 
     def jump(self, direction: str) -> None:
         """
@@ -141,17 +143,26 @@ class Frog:
         Args:
         direction (str): The direction to jump.
         """
+        self.direction = direction
         if not self.jumping:
             if direction == "north" and self.pos.y >= 100:
+                if self.old_direction != direction:
+                    self.fix_position()
                 self.destination.y = self.pos.y - self.JUMP_DISTANCE
             elif direction == "south" and self.pos.y <= 538:
+                if self.old_direction != direction:
+                    self.fix_position()
                 self.destination.y = self.pos.y + self.JUMP_DISTANCE
             elif direction == "west" and self.pos.x > self.JUMP_DISTANCE:
+                if self.old_direction != direction:
+                    self.fix_position()
                 self.destination.x = self.pos.x - self.JUMP_DISTANCE
             elif direction == "east" and self.pos.x < stgs.WINDOW_SIZE[0] - self.JUMP_DISTANCE:
+                if self.old_direction != direction:
+                    self.fix_position()
                 self.destination.x = self.pos.x + self.JUMP_DISTANCE
 
-        self.direction = direction
+        
         self.jumping = True          
 
     def render(self, surf: pg.Surface) -> None:
@@ -161,6 +172,7 @@ class Frog:
         surf (pg.Surface): The surface to render the frog on.
         """
         if self.rotate:
-            self.image = pg.transform.rotate(self.image, self.angle)
+            self.image_to_blit = pg.transform.rotate(self.image, self.angle)
             self.rotate = False
-        surf.blit(self.image, self.rect)
+        surf.blit(self.image_to_blit, self.image_rect)
+        pg.draw.rect(surf, "red", self.collision_rect)
